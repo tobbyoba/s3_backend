bc88cd3304835fb96fa63a16d71015e7e6f0e543
tobbyoba <tobbyoba86@gmail.com>
2024-01-22 16:42:09 -0600

    bucket

---
+++ bucket_deploy/s3-module.tf
@@ -0,0 +1,17 @@
+module "s3" {
+    source = "git@github.com:tobbyoba/GitAction_s3_backend.git//s3-module?ref=v1.2.0"
+  
+}
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "toba-bucket"
+    region  = "us-east-1"
+    key     = "s3/terraform.tfstate"
+    encrypt = "true"
+  }
+}
